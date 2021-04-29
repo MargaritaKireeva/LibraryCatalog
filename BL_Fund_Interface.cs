5a7f4555b815a76373a01cf741c0240ad008bf52
MargaritaKireeva <56782583+MargaritaKireeva@users.noreply.github.com>
2021-04-29 13:10:34 +0400

    Add files via upload

---
+++ BL_Fund_Interface.cs
@@ -0,0 +1,16 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BL_Interface
+{
+    public interface BL_Fund_Interface
+    {
+        void AddFund(Fund fund);
+        void DeleteFund(string FundName);
+        IEnumerable<Fund> GetAllFunds();
+    }
+}
